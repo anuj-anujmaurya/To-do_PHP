332a0b66da508ce543f04c27620f257984713368
Anuj Maurya <anuj.anujmaurya@gmail.com>
2023-07-05 17:38:24 +0530

    initial commit

---
+++ src/JS/main.js
@@ -0,0 +1,118 @@
+// function to display (all by-default)
+function display(param) {
+    $.ajax({
+        type: "POST",
+        url: "handler.php",
+        data: { 'type': 'display', 'param': param },
+        dataType: 'text',
+        success: function (res) {
+            $('.incomplete-list').html(res);
+        }
+    })
+}
+display('all');
+
+// function to count remaining
+function countRemaining() {
+    $.ajax({
+        type: "POST",
+        url: "handler.php",
+        data: { 'type': 'count_completed' },
+        dataType: 'text',
+        success: function (res) {
+            // display('all');
+            $('.todo-left').html("Remaining : " + res);
+        }
+    });
+}
+
+$(document).ready(function () {
+    // to add in todo
+    $(document).on('click', '.add-todo', function () {
+        title = $('#form3').val();
+        date = $('#datepicker').val();
+        $.ajax({
+            type: "POST",
+            url: "handler.php",
+            data: { 'type': 'add', 'title': title, 'date': date },
+            dataType: 'text',
+            success: function (res) {
+                display('all');
+                countRemaining();
+            }
+        })
+    })
+
+    // toggle the todo
+    $(document).on('click', '.toggle-item', function () {
+        idx = $(this).attr('id');
+        $.ajax({
+            type: "POST",
+            url: "handler.php",
+            data: { 'type': 'toggle', 'id': idx },
+            dataType: 'text',
+            success: function (res) {
+                display('all');
+                countRemaining();
+            }
+        })
+    })
+    // action on delete button
+    $(document).on('click', '.delete-btn', function () {
+        idx = $(this).attr('id');
+        $.ajax({
+            type: "POST",
+            url: "handler.php",
+            data: { 'type': 'delete', 'id': idx },
+            dataType: 'text',
+            success: function (res) {
+                display('all');
+                countRemaining();
+            }
+        })
+    })
+
+    // action on display all button
+    $(document).on('click', '.all', function () {
+        display('all');
+        countRemaining();
+    })
+
+    $(document).on('click', '#active', function () {
+        display('incomplete');
+        countRemaining();
+    })
+
+    $(document).on('click', '.completed', function () {
+        display('complete');
+        countRemaining();
+    })
+    // clear complete action
+    $(document).on('click', '.clear-complete', function () {
+        $.ajax({
+            type: "POST",
+            url: "handler.php",
+            data: { 'type': 'clear_completed' },
+            dataType: 'text',
+            success: function (res) {
+                display('all');
+                countRemaining();
+            }
+        })
+    });
+
+
+    // to edit
+    $(document).on('keyup', '.ip', function () {
+        val = $(this).val();
+        id = $(this).attr('id');
+        $.ajax({
+            type: "POST",
+            url: "handler.php",
+            data: { 'type': 'update', 'val': val, 'id': id },
+            dataType: 'text',
+        })
+    })
+    display('all');
+    countRemaining();
+});
---
+++ src/handler.php
@@ -0,0 +1,132 @@
+<?php
+session_start();
+if (!isset($_SESSION['count_active'])) {
+    $_SESSION['count_active'] = 0;
+}
+
+include('config.php');
+$type = $_POST['type'];
+
+// function to display all completed tasks
+function displayComplete($conn)
+{
+    $sql = "SELECT * FROM todoList WHERE `position` = 'inactive'";
+    $result = mysqli_query($conn, $sql);
+    $res = "";
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            $res .= "<li class=\"list-group-item d-flex just8080ify-content-between border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2\">
+                        <div class=\"d-flex\">
+                            <input class=\"form-check-input me-2 toggle-item\" type=\"checkbox\" checked id = '$row[id]'
+                                />
+                        </div>
+                        <input type=\"text\" id = '$row[id]' class = 'ip' name=\"lname\" disabled value = '$row[title]'>
+                        <button class = \"delete-btn btn-danger\" id = '$row[id]'>X</button>
+                    </li>";
+        }
+    }
+    return $res;
+}
+// function to display all incomplete tasks
+function displayIncomplete($conn)
+{
+    include_once('config.php');
+    $sql = "SELECT * FROM todoList WHERE `position` = 'active'";
+    $result = mysqli_query($conn, $sql);
+    $res = "";
+    $_SESSION['count_active'] = 0;
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            $_SESSION['count_active']++;
+            $res .= "<li class=\"list-group-item d-flex justify-content-between border-start-0 border-top-0 border-end-0 border-bottom rounded-0 mb-2\">
+                        <div class=\"d-flex\">
+                            <input class=\"form-check-input me-2 toggle-item\" type=\"checkbox\" id = '$row[id]'
+                                />
+                        </div>
+                        <input type=\"text\" id = '$row[id]' class = 'ip' name=\"lname\" value = '$row[title]'>
+                        <button class = \"delete-btn btn-danger\" id = '$row[id]'>X</button>
+                    </li>";
+        }
+    }
+    return $res;
+}
+switch ($type) {
+        // to add new task
+    case 'add':
+        // date is yet not added
+        $title = $_POST['title'];
+        $dt = $_POST['date'];
+        $sql = "INSERT INTO todoList (`title`, `ex_date`)
+                    VALUES ('$title', '$dt')";
+        $result = mysqli_query($conn, $sql);
+        echo "<pre></pre>";
+        print_r($sql);
+        break;
+
+        // to display all tasks
+    case 'display':
+        $param = $_POST['param'];
+        switch ($param) {
+            case 'complete':
+                echo displayComplete($conn);
+                break;
+
+            case 'incomplete':
+                echo displayIncomplete($conn);
+                break;
+
+            default:
+                echo displayIncomplete($conn);
+                echo displayComplete($conn);
+                break;
+        }
+
+        // to toggle between complete and incomplete tasks
+    case 'toggle':
+        if (isset($_POST['id'])) {
+            $idx = $_POST['id'];
+            $sql = "SELECT `position` FROM todoList WHERE `id` = '$idx'";
+            $res = mysqli_query($conn, $sql);
+
+            $row = mysqli_fetch_assoc($res);
+            $curr_status = $row['position'];
+            $dest = "active";
+            if ($curr_status == 'active') {
+                $dest = 'inactive';
+            }
+            $sql = "UPDATE todoList SET `position` = '$dest' WHERE `id` = '$idx'";
+            $res = mysqli_query($conn, $sql);
+        }
+        break;
+        // to delete a task
+    case 'delete':
+        $id = $_POST['id'];
+        $sql = "DELETE FROM `todoList` WHERE `id` = '$id'";
+        $res = mysqli_query($conn, $sql);
+        echo "<pre></pre>";
+        print_r($_POST);
+        echo $id;
+        break;
+
+        // to clear all completed
+    case 'clear_completed':
+        $sql = "DELETE FROM `todoList` WHERE `position` = 'inactive'";
+        $res = mysqli_query($conn, $sql);
+        break;
+        // to update a particular incomplete task
+    case 'update':
+        $val = $_POST['val'];
+        $id = $_POST['id'];
+        $sql = "UPDATE `todoList` SET `title`='$val' WHERE `id` = '$id'";
+        $res = mysqli_query($conn, $sql);
+        break;
+        // to find the count of all incomplete tasks
+    case 'count_completed':
+        displayIncomplete($conn);
+        echo $_SESSION['count_active'];
+        break;
+    default:
+        break;
+}
